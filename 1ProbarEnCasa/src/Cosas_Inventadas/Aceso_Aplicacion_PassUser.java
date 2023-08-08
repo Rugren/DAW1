fc51f1ab6932ae091a224a0b8c39699a9cc620f7
Rugren <117831601+Rugren@users.noreply.github.com>
2023-08-08 14:38:45 +0200

    Add files via upload

--- 1ProbarEnCasa/src/Cosas_Inventadas/Aceso_Aplicacion_PassUser.java
+++ 1ProbarEnCasa/src/Cosas_Inventadas/Aceso_Aplicacion_PassUser.java
@@ -12,7 +12,17 @@ public class Aceso_Aplicacion_PassUser {
 		
 		while (clave.equals(pass)== false) {
 			
-			pass = JOptionPane.showInputDialog("Introduce la contraseña, por favor");
+			// solucionar lo de que cuando pongo nombre correcto, me pone: "Nombre incorrecto"
+			clave = JOptionPane.showInputDialog("Introduce tu nombre, por favor");
+			if (clave.equals(clave)==false) {
+				System.out.println("Nombre correcto");
+			}
+			
+			else if (clave.equals(clave)==true) {
+				System.out.println("Nombre incorrecto");
+			}		
+			
+			clave = JOptionPane.showInputDialog("Introduce la contraseña, por favor");
 					
 			if (clave.equals(pass)==false) {
 				System.out.println("Contraseña incorrecta");
@@ -25,3 +35,29 @@ public class Aceso_Aplicacion_PassUser {
 	}
 
 }
+
+/* // como estaba antes+ (este si iba, 
+ * cambiar el: "pass = JOptionPane.showInputDialog("Introduce la contraseña, por favor");"
+ * por este: "clave = JOptionPane.showInputDialog("Introduce la contraseña, por favor");"
+ */
+
+/* public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	
+	String clave = "Juan";
+	String pass = "xd";
+	
+	while (clave.equals(pass)== false) {
+		
+		pass = JOptionPane.showInputDialog("Introduce la contraseña, por favor");
+				
+		if (clave.equals(pass)==false) {
+			System.out.println("Contraseña incorrecta");
+		} // cierre del if
+	} // cierre del while
+	
+	System.out.println("Contraseña correcta. Acceso Permitido");
+	
+
+}
+*/
