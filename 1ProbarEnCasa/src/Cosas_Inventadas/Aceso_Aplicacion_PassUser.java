8bb7358f9da2dff04b1847311351715595c7b702
Rugren <117831601+Rugren@users.noreply.github.com>
2023-08-08 14:54:17 +0200

    Add files via upload

--- 1ProbarEnCasa/src/Cosas_Inventadas/Aceso_Aplicacion_PassUser.java
+++ 1ProbarEnCasa/src/Cosas_Inventadas/Aceso_Aplicacion_PassUser.java
@@ -10,9 +10,11 @@ public class Aceso_Aplicacion_PassUser {
 		String clave = "Juan";
 		String pass = "xd";
 		
+
 		while (clave.equals(pass)== false) {
 			
 			// solucionar lo de que cuando pongo nombre correcto, me pone: "Nombre incorrecto"
+			// entra con la contrasena sea cual sea el nombre.
 			clave = JOptionPane.showInputDialog("Introduce tu nombre, por favor");
 			if (clave.equals(clave)==false) {
 				System.out.println("Nombre correcto");
@@ -60,4 +62,60 @@ public class Aceso_Aplicacion_PassUser {
 	
 
 }
+
+
+Con el while solo sería: 
+		while (clave.equals(pass)== false) {
+			
+			// solucionar lo de que cuando pongo nombre correcto, me pone: "Nombre incorrecto"
+			clave = JOptionPane.showInputDialog("Introduce tu nombre, por favor");
+			if (clave.equals(clave)==false) {
+				System.out.println("Nombre correcto");
+			}
+			
+			else if (clave.equals(clave)==true) {
+				System.out.println("Nombre incorrecto");
+			}		
+			
+			clave = JOptionPane.showInputDialog("Introduce la contraseña, por favor");
+					
+			if (clave.equals(pass)==false) {
+				System.out.println("Contraseña incorrecta");
+			} // cierre del if
+		} // cierre del while
+		
+		System.out.println("Contraseña correcta. Acceso Permitido");
+		
+
+	}
+	
+Con el do while sería: (quitando el "{" y poniendo ";" )
+		do {
+			clave = JOptionPane.showInputDialog("Introduce tu nombre, por favor");
+		}
+		
+		while (clave.equals(pass)== false);
+			
+			// solucionar lo de que cuando pongo nombre correcto, me pone: "Nombre incorrecto"
+			clave = JOptionPane.showInputDialog("Introduce tu nombre, por favor");
+			if (clave.equals(clave)==false) {
+				System.out.println("Nombre correcto");
+			}
+			
+			else if (clave.equals(clave)==true) {
+				System.out.println("Nombre incorrecto");
+			}		
+			
+			clave = JOptionPane.showInputDialog("Introduce la contraseña, por favor");
+					
+			if (clave.equals(pass)==false) {
+				System.out.println("Contraseña incorrecta");
+			} // cierre del if
+		
+		System.out.println("Contraseña correcta. Acceso Permitido");
+		
+
+	}
+	
+
 */
